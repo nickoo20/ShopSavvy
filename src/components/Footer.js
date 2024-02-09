18c44182f97afdeadbdd1de777e51c65c649fb9a
nickoo20 <gniyati20@gmail.com>
2024-02-09 11:53:27 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className='bg-primary py-4 '>
+    <div className='container mx-auto'>
+      <p className='text-white text-end . All rights reserved. '> | NIYATI GUPTA | 2023</p>
+    </div>
+
+  </footer>;
+};
+
+export default Footer;
